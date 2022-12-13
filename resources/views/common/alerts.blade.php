252990e320feefc50ef193bb1fcdde639f5bb87c
Ancora <anderson.rocha@ancora-ti.com.br>
2022-12-13 18:23:27 -0300

    Alterar intensidade das cores das mensagens.

--- resources/views/common/alerts.blade.php
+++ resources/views/common/alerts.blade.php
@@ -1,23 +1,23 @@
 @if ($message = Session::get('success'))
-    <div class="alert alert-success alert-block text-green-400">
+    <div class="alert alert-success alert-block text-green-600">
         <button type="button" class="close" data-dismiss="alert"></button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
 @if ($message = Session::get('error'))
-    <div class="alert alert-danger alert-block text-red-400">
+    <div class="alert alert-danger alert-block text-red-600">
         <button type="button" class="close" data-dismiss="alert"></button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
 @if ($message = Session::get('warning'))
-    <div class="alert alert-warning alert-block text-yellow-400">
+    <div class="alert alert-warning alert-block text-yellow-600">
         <button type="button" class="close" data-dismiss="alert"></button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
 @if ($message = Session::get('info'))
-    <div class="alert alert-info alert-block text-blue-400">
+    <div class="alert alert-info alert-block text-blue-600">
         <button type="button" class="close" data-dismiss="alert"></button>
         <strong>{{ $message }}</strong>
     </div>
